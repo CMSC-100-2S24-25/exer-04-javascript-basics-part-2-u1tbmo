82e8a430e30676e4c25034cc3af3570720b12b3a
Euan Jed Tabamo <estabamo1@up.edu.ph>
2025-02-25 14:33:45 +0800

    feat: add ability to add accounts to a file

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ CMSC 100 C1L
 */
 
 import { v7 as uuidv7 } from "uuid";
+import validator from "validator";
+import { appendFileSync } from "node:fs";
 
 function generateUniqueId(firstName, lastName) {
 	// Check string lengths
@@ -17,4 +19,36 @@ function generateUniqueId(firstName, lastName) {
 
 	// Return the unique id using the pattern:
 	return `${firstName.toLowerCase().trim()[0]}${lastName.toLowerCase().trim()}${uniqueString}`;
+}
+
+function addAccount(firstName, lastName, email, age) {
+	// Check string lengths and value of age
+	if (
+		firstName.length <= 0 ||
+		lastName.length <= 0 ||
+		email.length <= 0 ||
+		age <= 18
+	) {
+		return false;
+	}
+
+	// Check if the email is valid
+	if (!validator.isEmail(email)) {
+		return false;
+	}
+
+	// Generate the unique id
+	const uniqueId = generateUniqueId(firstName, lastName);
+
+	// Save the data into users.txt
+	try {
+		const data = `${firstName},${lastName},${email},${age},${uniqueId}`;
+		appendFileSync("users.txt", data);
+	} catch (err) {
+		// If an error was encountered, return false
+		return false;
+	}
+
+	// Return true to indicate success
+	return true;
 }
