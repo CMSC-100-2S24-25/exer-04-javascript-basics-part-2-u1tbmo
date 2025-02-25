ca4ffd689f15bead92336f13e0d310bfcb471917
Euan Jed Tabamo <estabamo1@up.edu.ph>
2025-02-25 14:06:11 +0800

    feat: add ability to generate unique ids

--- index.js
+++ index.js
@@ -3,3 +3,17 @@
 February 25, 2025
 CMSC 100 C1L
 */
+
+import { v7 as uuidv7 } from "uuid";
+
+function generateUniqueId(firstName, lastName) {
+	if (firstName.length <= 0 || lastName.length <= 0) {
+		return null;
+	}
+
+	// Get the first 8 characters from the uuid
+	const uniqueString = uuidv7().split("-")[0];
+
+	// Return the unique id using the pattern:
+	return `${firstName.toLowerCase().trim()[0]}${lastName.toLowerCase().trim()}${uniqueString}`;
+}
